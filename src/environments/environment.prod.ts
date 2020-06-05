7f38f982bfd67ab85e92f3aa5c868c126f9d7c7a
evgeniy.klychko <evgeniy.klychko@gmail.com>
2020-06-05 17:25:52 +0300

    rebuild

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAt3giWV5IWj0FMKWn7oO47ZhcKkgzoTWs',
+    authDomain: 'ocloud-77b52.firebaseapp.com',
+    databaseURL: 'https://iocloud-77b52.firebaseio.com/',
+    projectId: 'iocloud-77b52',
+    storageBucket: '<your-storage-bucket>',
+    messagingSenderId: '267434257483'
+  }
 };
